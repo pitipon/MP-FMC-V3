38371a6cb377168a48f2bef3edcc9e8621dc668a
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-04-23 17:06:33 +0800

    form working

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,66 +1,28 @@
 // pages/post/post.js
+
+const app = getApp()
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
   
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // Bind Submit
+  bindSubmit: function (e) {
+ 
+    wx.showToast({
+      title: 'Sending...',
+      icon: 'loading',
+      duration: 1500
+    })
+    let name = e.detail.value.name
+    let content = e.detail.value.content
+    
+    console.log(e)
+    app.globalData.items.unshift({name: name, content: content})
+    
+  },
   onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
   
   }
 })
